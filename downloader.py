60160ec7bde0b4e0e1a6d2a324efd91672390593
whiteSkar <sypower07@hotmail.com>
2016-09-05 13:33:55 -0700

    Can now download the whole episode

--- downloader.py
+++ downloader.py
@@ -1,17 +1,44 @@
+from html.parser import HTMLParser
 import requests
 
-url = 'http://imgcomic.naver.net/webtoon/183559/82/20120203183452_IMAG01_1.jpg'
-headers = {'referer': 'http://comic.naver.com/webtoon/detail.nhn?titleId=183559&no=82&weekday=mon'}
+
+imgs_to_dl = []
 
 
-r = requests.get(url, headers=headers)
+class NaverWebtoonParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        global imgs_to_dl
 
-with open('test.jpg', 'wb') as outfile:
-    outfile.write(r.content)
+        if (tag == "img"):
+            if len(attrs) > 2 and len(attrs[0]) > 0 and attrs[0][0] == 'src' and attrs[2][0] == 'alt' and attrs[2][1] == 'comic content':
+                imgs_to_dl.append(attrs[0][1])
 
-if r.status_code != 200:
-    print("Get request failed")
+
+webtoon_id = 183559
+start_id = 296
+
+webtoon_ep_url = 'http://comic.naver.com/webtoon/detail.nhn?titleId={}&no={}&weekday=mon'.format(webtoon_id, start_id)
+
+ep_main_page_r = requests.get(webtoon_ep_url)
+if ep_main_page_r.status_code != 200:
+    print("Get request for episode main page failed")
     exit()
 
-with open('test.jpg', 'wb') as out_file:
-    out_file.write(r.content)
+parser = NaverWebtoonParser()
+parser.feed(ep_main_page_r.text)
+
+print("Images to download are:")
+for img_url in imgs_to_dl:
+    print(img_url)
+
+headers = {'referer': 'http://comic.naver.com/webtoon/detail.nhn?titleId=183559&no=82'}
+
+for i in range(len(imgs_to_dl)):
+    r = requests.get(imgs_to_dl[i], headers=headers)
+    if r.status_code != 200:
+        print("Get request failed")
+    
+    img_file_name = '%03d.jpg' % (i,)
+    with open(img_file_name, 'wb') as outfile:
+        outfile.write(r.content)
+
