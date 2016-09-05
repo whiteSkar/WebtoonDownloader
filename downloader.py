53e315f7be0ff40b755949543862eff7bcbfe055
whiteSkar <sypower07@hotmail.com>
2016-09-05 14:12:27 -0700

    Download from the specified episode to the latest one

--- downloader.py
+++ downloader.py
@@ -23,39 +23,49 @@ class NaverWebtoonParser(HTMLParser):
         if self.get_starttag_text() == "<h3>" and data.strip():
             webtoon_title = data
 
+def download_ep(webtoon_id, ep_id):
+    webtoon_ep_url = 'http://comic.naver.com/webtoon/detail.nhn?titleId={}&no={}&weekday=mon'.format(webtoon_id, ep_id)
+
+    ep_main_page_r = requests.get(webtoon_ep_url)
+    if ep_main_page_r.status_code != 200:
+        print("Get request for episode main page failed")
+        exit()
+
+    parser = NaverWebtoonParser()
+    parser.feed(ep_main_page_r.text)
+
+    if len(imgs_to_dl) == 0:
+        print("There are no images to download. Probably this ep is not released yet.")
+        return False
+
+    #print("Images to download are:")
+    #for img_url in imgs_to_dl:
+    #    print(img_url)
+
+    headers = {'referer': 'http://comic.naver.com/webtoon/detail.nhn?titleId=183559&no=82'}
+
+    # ep_id for sorting purposes
+    folder_path = ('%04d_' % (ep_id,)) + webtoon_title
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    for i in range(len(imgs_to_dl)):
+        r = requests.get(imgs_to_dl[i], headers=headers)
+        if r.status_code != 200:
+            print("Get request failed")
+        
+        img_file_name = '%03d.jpg' % (i,)
+        with open(folder_path + '/' + img_file_name, 'wb') as outfile:
+            outfile.write(r.content)
+
+    return True
 
 webtoon_id = 183559
-start_id = 296
+start_id = 1
 
-webtoon_ep_url = 'http://comic.naver.com/webtoon/detail.nhn?titleId={}&no={}&weekday=mon'.format(webtoon_id, start_id)
+while download_ep(webtoon_id, start_id):
+    print("Downloading " + webtoon_title + "complete.")
+    imgs_to_dl = []
+    start_id += 1
 
-ep_main_page_r = requests.get(webtoon_ep_url)
-if ep_main_page_r.status_code != 200:
-    print("Get request for episode main page failed")
-    exit()
-
-parser = NaverWebtoonParser()
-parser.feed(ep_main_page_r.text)
-
-print("Images to download are:")
-#for img_url in imgs_to_dl:
-#   print(img_url)
-
-headers = {'referer': 'http://comic.naver.com/webtoon/detail.nhn?titleId=183559&no=82'}
-
-# start_id for sorting purposes
-folder_path = ('%04d_' % (start_id,)) + webtoon_title
-if not os.path.exists(folder_path):
-    os.makedirs(folder_path)
-
-print(webtoon_title)
-print(folder_path)
-for i in range(len(imgs_to_dl)):
-    r = requests.get(imgs_to_dl[i], headers=headers)
-    if r.status_code != 200:
-        print("Get request failed")
-    
-    img_file_name = '%03d.jpg' % (i,)
-    with open(folder_path + '/' + img_file_name, 'wb') as outfile:
-        outfile.write(r.content)
-
+print("Downloading the webtoon complete.")
