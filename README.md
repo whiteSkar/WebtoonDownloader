229c9853b012b8b5cca7993e3638c268e7172475
whiteSkar <sypower07@hotmail.com>
2016-09-06 22:13:48 -0700

    Now it will keep downloading episodes until the newest one even if there is a skipped episode number in between

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # WebtoonDonwloader
 
 Things to be done
-1. There are some episodes that are skipped. Should continue downloading from the next one.
-2. Add GUI
+- Add GUI
+- Clean up code
--- downloader.py
+++ downloader.py
@@ -7,23 +7,38 @@ import requests
 # Globals
 webtoon_title = ""
 imgs_to_dl = []
+newest_ep_id = 0
 
 
 # Classes
-class NaverWebtoonParser(HTMLParser):
+class NaverWebtoonEpParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         global imgs_to_dl
 
-        if (tag == "img"):
+        if (tag == 'img'):
             if len(attrs) > 2 and len(attrs[0]) > 0 and attrs[0][0] == 'src' and len(attrs[2]) > 1 and attrs[2][0] == 'alt' and attrs[2][1] == 'comic content':
                 imgs_to_dl.append(attrs[0][1])
-
+    
     def handle_data(self, data):
         global webtoon_title
         
         # Yay there is only one tag that starts with h3!
         if self.get_starttag_text() == "<h3>" and data.strip():
             webtoon_title = data
+
+class NaverWebtoonListPageParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        global newest_ep_id
+        
+        if (newest_ep_id == 0 and tag == 'a'):  # Lol.. too nested yo
+            if len(attrs) == 2 and len(attrs[0]) > 0 and attrs[0][0] == 'href': # len(attrs) == 2 is hacky but easy way to bypass '첫회보기' link
+                ep_no_identifier = 'no='
+                pos_ep_no = attrs[0][1].find(ep_no_identifier)
+                if pos_ep_no >= 0:
+                    pos_ep_no_end = attrs[0][1].find('&', pos_ep_no)
+                    if pos_ep_no_end == -1:
+                        pos_ep_no_end = len(attrs[0][1])
+                    newest_ep_id = int(attrs[0][1][pos_ep_no + len(ep_no_identifier) : pos_ep_no_end]) # You know.. Not gonna error check
 
 
 # Functions
@@ -35,7 +50,7 @@ def download_ep(directory_path, webtoon_id, ep_id):
         print("Get request for episode main page failed")
         exit()
 
-    parser = NaverWebtoonParser()
+    parser = NaverWebtoonEpParser()
     parser.feed(ep_main_page_r.text)
 
     if len(imgs_to_dl) == 0:
@@ -73,9 +88,23 @@ start_id = input('Input the episode id of the first episode you want to download
 webtoon_id = int(webtoon_id)
 start_id = int(start_id)
 
-while download_ep(directory_path, webtoon_id, start_id):
-    print("Downloading " + webtoon_title + "complete.")
-    imgs_to_dl = []
+webtoon_list_page_url = 'http://comic.naver.com/webtoon/list.nhn?titleId={}'.format(webtoon_id)
+
+webtoon_list_r = requests.get(webtoon_list_page_url)
+if webtoon_list_r.status_code != 200:
+    print("Get request for webtoon list page failed")
+    exit()
+
+parser = NaverWebtoonListPageParser()
+parser.feed(webtoon_list_r.text)
+
+print("Downloading webtoon from ep_id:" + str(start_id) + " to ep_id:" + str(newest_ep_id) + " started.")
+while start_id <= newest_ep_id:
+    if download_ep(directory_path, webtoon_id, start_id):
+        print("Downloading " + webtoon_title + "complete.")
+        imgs_to_dl = []
+    else:
+        print("Episode #" + str(start_id) + " doesn't exist. Skipping.")
     start_id += 1
 
 print("Downloading the webtoon complete.")
